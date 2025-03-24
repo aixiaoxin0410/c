ac1a0dcd00542145d4e462374f890cd4c475cb59
xiaoxin <1299037599@qq.com>
2025-03-24 09:21:15 +0800

    同下

--- core/chapter4/chapter4.10.cpp
+++ core/chapter4/chapter4.10.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
 
+//class House; // 前置声明，为函数做友元准备
+
 class Building
 {
     friend void goodfriend(Building *building); // 全局函数做友元
     friend class House; // 类做友元
+    //friend void House::visitbuilding(Building *building); // 函数做友元
+
+
 public:
     string m_sittingRoom;
 
@@ -30,11 +35,24 @@ public:
         housepos = "北边";building = new Building(); // 初始化 building 指针
     }
 
-    void visithouse(House *house)
+    // 添加析构函数，释放内存
+    ~House()
     {
-        cout << "朋友正在访问: " << house->housepos << endl;
-        cout << "朋友正在访问: " << house->building->m_sittingRoom << endl;
-        cout << "朋友正在访问: " << house->building->m_bedRoom << endl;
+        delete building;
+    }
+
+
+    // void visithouse(House *house)
+    // {
+    //     cout << "朋友正在访问的位置是: " << house->housepos << endl;
+    //     cout << "朋友正在访问: " << house->building->m_sittingRoom << endl;
+    //     cout << "朋友正在访问: " << house->building->m_bedRoom << endl;
+    // }
+
+    void visitbuilding(Building *building)
+    {
+        cout << "朋友正在访问: " << building->m_sittingRoom << endl;
+        cout << "朋友正在访问: " << building->m_bedRoom << endl;
     }
 
 };
@@ -50,7 +68,9 @@ int main()
     Building building1;
 
     House house1;
-    house1.visithouse(&house1);
+    // house1.visithouse(&house1);
+    house1.visitbuilding(&building1);
+
     system("cls");
     return 0;
 }
